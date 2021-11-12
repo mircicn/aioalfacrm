423be891e793df3a00834fabf5f01aa8612c51ff
Stanislav Rush <911rush@gmail.com>
2021-11-12 22:08:16 +0700

    Add room api object

--- aioalfacrm/client.py
+++ aioalfacrm/client.py
@@ -55,6 +55,7 @@ class AlfaClient:
         self.lead_source = crud_objects.LeadSource(self.api_client, models.LeadSource)
         self.group = crud_objects.Group(self.api_client, models.Group)
         self.lesson = crud_objects.Lesson(self.api_client, models.Lesson)
+        self.room = crud_objects.Room(self.api_client, models.Room)
 
     @classmethod
     def _create_session(cls) -> aiohttp.ClientSession:
